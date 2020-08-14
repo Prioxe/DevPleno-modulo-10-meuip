6b5331e981315fa42f001f8bff33a1cdd1d63af8
Prioxe <36611417+Prioxe@users.noreply.github.com>
2020-08-14 08:32:20 -0300

    01 - Inicio do projeto.

---
+++ meuip/App.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+
+} from 'react-native';
+
+
+const App: () => React$Node = () => {
+  return (
+    <>
+    <View>
+      <Text>Olá</Text>
+    </View>
+      
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
